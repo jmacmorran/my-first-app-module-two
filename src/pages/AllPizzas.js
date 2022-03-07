2f390a58b050825677481bda2d7de3fd650a887f
Joseph MacMorran <Joseph@Josephs-MBP.attlocal.net>
2022-03-07 17:40:15 -0600

    added dummy data

--- src/pages/AllPizzas.js
+++ src/pages/AllPizzas.js
@@ -2,9 +2,31 @@ import { useState, useEffect } from "react";
 
 import PizzaList from "../components/pizzas/PizzaList";
 
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
+
+
 function AllPizzas() {
   const [isLoading, setIsLoading] = useState(true);
-  const [loadedpizzas, setLoadedpizzas] = useState([]);
+  const [loadedpizzas, setLoadedpizzas] = useState(DUMMY_DATA);
 
   useEffect(() => {
     setIsLoading(true);
@@ -27,7 +49,7 @@ function AllPizzas() {
         }
 
         setIsLoading(false);
-        setLoadedpizzas(pizzas);
+        // setLoadedpizzas(pizzas);
       });
   }, []);
 
